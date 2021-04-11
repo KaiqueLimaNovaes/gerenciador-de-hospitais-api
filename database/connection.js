af5ea7c190018f4ff338db5b14ed1477196720de
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-04-10 23:49:10 -0300

    Conexão com o container docker mysql

--- database/connection.js
+++ database/connection.js
@@ -2,11 +2,13 @@ var knex = require("knex")({
     
     client: "mysql2",
     connection: {
-        host: "localhost",
+        //host: "localhost",
+        host: "http://192.168.0.25:3306",
         user: "root",
-        //port: "3005",
-        password: "",
-        database: "gerenciador_hospitais",
+        port: "3306",
+        password: "30011995",
+        //database: "gerenciador_hospitais",
+        database: "gerenciadorDeHospitais",
     },
 });
   
